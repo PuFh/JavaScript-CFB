779aa67dcdad0ef8100132e5a5a812ce486a8a00
Matheus <137668343+PuFh@users.noreply.github.com>
2025-04-15 19:46:45 -0400

    Operador Typeof, retornando o tipo da variável

---
+++ Exercicios/aula11.js
@@ -0,0 +1,10 @@
+//Operador Typeof, retornando o tipo da variável
+let v1 = 10
+let v2 = "10"
+let v3 = v1===v2//Tipo boolean: que seria verdadeiro ou falso
+let v4 = {nome: "Bruno"}//Isso aqui é um objeto
+
+console.log("Valor: "+v1+" - Tipo: "+typeof(v1))
+console.log("Valor: "+v2+" - Tipo: "+typeof(v2))
+console.log("Valor: "+v3+" - Tipo: "+typeof(v3))
+console.log("Valor: "+v4+" - Tipo: "+typeof(v4))
