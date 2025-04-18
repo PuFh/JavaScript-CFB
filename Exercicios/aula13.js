9a5d585bd8686c396ff6f2b8f4b1c1c7109bde7f
Matheus <137668343+PuFh@users.noreply.github.com>
2025-04-18 11:21:35 -0400

    Update aula13.js

---
+++ Exercicios/aula13.js
@@ -0,0 +1,9 @@
+//Comando condicional IF e IF ELSE em Javascript
+
+let num=11
+
+if(num > 10)
+    console.log("Numeral maior que 10")
+    console.log("comando 2")
+
+console.log("Fim do programa")
