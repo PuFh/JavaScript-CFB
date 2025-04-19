83e182affaab3e591932bcb0d805b983765b9457
Matheus <137668343+PuFh@users.noreply.github.com>
2025-04-18 22:15:36 -0400

    Comando Switch Case em Javascript

--- Exercicios/aula13.js
+++ Exercicios/aula13.js
@@ -1,9 +1,13 @@
 //Comando condicional IF e IF ELSE em Javascript
 
-let num=11
+let energia=70
+let clima = "sol"
 
-if(num > 10)
-    console.log("Numeral maior que 10")
-    console.log("comando 2")
+if(energia >=70 && clima =="sol"){
+    console.log("Vou a praia")
+   
+}else{
+    console.log("Vou ao cinema")
+}
 
 console.log("Fim do programa")
---
+++ Exercicios/aula14.js
@@ -0,0 +1,1 @@
+//Comando Switch Case em Javascript
