a6ff065de35728ea9e3bb527876e57977696c253
Matheus <137668343+PuFh@users.noreply.github.com>
2025-04-24 11:31:11 -0400

    Loop FOR em Javascript

--- Exercicios/aula14.js
+++ Exercicios/aula14.js
@@ -1,1 +1,19 @@
 //Comando Switch Case em Javascript
+let colocacao = 1
+
+switch(colocacao){
+    case 1:
+        console.log("Primeiro Lugar")
+        break
+    case 2:
+        console.log("Segundo Lugar")
+        break
+    case 3:
+        console.log("Terceiro Lugar")
+        break
+    case 4: case 5: case 6:
+        console.log("Prêmio de participação")
+    default:
+        console.log("Não subiu ao podio")
+        break
+}
