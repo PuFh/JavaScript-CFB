379522e8b90060473319623b811995336b118970
Matheus <137668343+PuFh@users.noreply.github.com>
2025-04-24 20:30:56 -0400

    aprendendo for

---
+++ Exercicios/aula15.js
@@ -0,0 +1,13 @@
+//Loop FOR em Javascript
+
+for(let i=0; i < 10; i++){
+    if(i%2==0){
+        console.log(i+" é Par")
+    }else if(i==2){
+        console.log(i+" é um numero neutro")
+    }else{
+        console.log(i+ " é impar")
+    }
+    /*O for só ira executar quando a sua condição for verdadeira, nesse caso, ele se mantera verdadeiro até o 9 */
+}
+console.log("Fim do programa")
