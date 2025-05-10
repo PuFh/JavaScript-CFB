d89b77df1950beefe2da34e84d4e2407bedaaa04
Matheus <137668343+PuFh@users.noreply.github.com>
2025-05-09 22:25:19 -0400

    learning about MAP

---
+++ Exercicios/aula28.js
@@ -0,0 +1,24 @@
+const converterInt=(e)=>parseInt(e)
+
+
+// const el=document.getElementsByTagName("div")
+// const valores=Array.prototype.map.call(el,({innerHTML})=>innerHTML)
+// console.log(valores)
+
+
+
+//Método MAP
+
+// const cursos=['HTML','Javascript','React','PHP']
+
+// const map1 = cursos.map((el,i)=>{
+//     return "Curso:"+el+" Posição: "+i
+// });
+
+// console.log(map1)
+// let map1=document.getElementsByTagName("div")
+// map1=[...map1]
+// console.log(map1)
+// map1.map((e,l)=>{
+//     console.log(e.innerHTML)
+// })
